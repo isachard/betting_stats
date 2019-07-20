bb45c3a5311ad29439390d7282c6294f03b75217
isachard <isachard.rodriguez@.pcc.edu>
2019-07-20 14:07:01 -0700

    fixes some bugs, theres some bugs on sf stats ( shows 6, instead of 7)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # betting_stats
+
+Installation:
+  $ pip install virtualenv
+ 
+Running the Script:
+  $ source env/bin/activate
+ 
+Exiting the enviorment:
+  $ deactive
--- test.py
+++ test.py
@@ -16,44 +16,44 @@ url = "https://www.covers.com/pageLoader/pageLoader.aspx?page=/data/mlb/teams/pa
 
 
 def format_url(_url):
-  result = requests.get(_url)
-  content = result.content
-  soup = BeautifulSoup(content, features="html.parser")
-  matches = soup.find_all('tr')
+    result = requests.get(_url)
+    content = result.content
+    soup = BeautifulSoup(content, features="html.parser")
+    matches = soup.find_all('tr')
 
-  last_update(matches)
+    last_update_date(matches)
 
+    team = soup.find('h1').text
 
-  team = soup.find('h1').text
-
-  
-
-  team_name(team)
-  record(matches)
-  total(matches)
-  streaks_ml(matches)
-  streaks_totals(matches)
+    team_name(team)
+    record(matches)
+    total(matches)
+    streaks_ml(matches)
+    streaks_totals(matches)
 
 
+def last_game_update(match):
+    return match[1].text.split()[0]
 
-def last_update(match):
 
-    print(match[1].text.split()[0])
+def last_update_date(match):
+    print()
+    print("Last updated: " + str(last_game_update(match)), end='')
 
 
 def urlHandler():
     global urlM
     for teams in range(30):
-        teamsURL = urlM + str(teams+teamdb) +".html"
+        teamsURL = urlM + str(teams+teamdb) + ".html"
         format_url(teamsURL)
-        
+
         #result = requests.get(url)
         #content = result.content
         #soup = BeautifulSoup(content, features="html.parser")
         #matches = soup.find_all('tr')
         #team = soup.find('h1').text
 
-        #team_name()
+        # team_name()
 
 
 def team_name(team):
@@ -90,7 +90,6 @@ def record(matches):
     total = win + lose
     print(str("%.2f" % ((win/total)*100)) + "% - " +
           str(("%.2f" % ((lose/total)*100))) + " %")
-    print()
 
 
 def total(matches):
@@ -122,7 +121,6 @@ def total(matches):
     total = over + under
     print(str("%.2f" % ((over/total)*100)) + "% - " +
           str(("%.2f" % ((under/total)*100))) + " %")
-    print()
 
 
 def streaks_totals(matches):
@@ -177,6 +175,7 @@ def streaks_ml(matches):
         location = len(games.text.split())
 
         if (location == 16):  # away
+            # print(games.text.split()[3])
 
             result = games.text.split()[11]
 
@@ -187,6 +186,7 @@ def streaks_ml(matches):
                 loss += 1
 
         if (location == 15):  # home
+            # print(games.text.split()[2])
 
             result = games.text.split()[10]
 

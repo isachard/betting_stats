91453d53cdcc9bc970a875928d8de020a2364f6d
isachard <isachard.rodriguez@pcc.edu>
2019-07-27 13:49:53 -0700

    added new branch for front view

--- view.py
+++ view.py
@@ -6,8 +6,10 @@ from tabulate import tabulate
 
 def tabulation():
     # print(tabulate([['ARI', 'W3', 'U4', '\u2713']], [headers=['Teams', 'Streak: W/L',  'Streak: O/U', 'LastUpdate']))
-    print(tabulate([['ARI', 'W3', 'L3', '\u2714']], headers=[
-          'Teams', 'S: W/L',  'S: O/U', 'LastUpdate']))
+    print(tabulate([['ARI', 'W3 - 53.5 %', 'L3 - 39.4%', '\u2714'],
+                    ['LAD', 'L2 - 43.5 %', 'W7 - 59.1%', '\u2716']],
+                   headers=[
+        'Teams', 'S: W/L -> %',  'S: O/U -> %', 'LastUpdate']))
 
 
 print()

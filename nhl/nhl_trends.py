c1544d253d35a4fe664769838794dc58af06fb9c
isachard <isachard.rodriguez@pcc.edu>
2019-10-18 15:46:49 -0700

    fixed folder structure

---
+++ nhl/nhl_trends.py
@@ -0,0 +1,83 @@
+import requests
+
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+teamdb = 55
+
+def url_loop():
+    file = open("links.txt","r")
+    links = file.readlines()
+    file.close()
+    return links
+
+def format_url(_url):
+
+    #put this in a function!
+    team_name = _url[57].upper() + _url[58:]
+    print(team_name)
+    result = requests.get(_url)
+    content = result.content
+    soup = BeautifulSoup(content, features="html.parser")
+    return soup
+
+def caller():
+    soup = format_url()
+    lista = get_totals_team(soup)
+    print(lista)
+
+def get_totals_teams(soup):
+    over = 0
+    under = 0
+    push = 0
+
+    matches = soup.find_all('td', class_=["viCellBg1 cellBorderR1 cellTextNorm padLeft","viCellBg2 cellBorderR1 cellTextNorm padLeft"])
+    for i in matches:
+
+        i = i.text.strip()
+        if (len(i) > 0):
+            if(i[0] == 'O'):
+                over +=1
+            if(i[0] == 'U'):
+                under +=1
+            if(i[0] == 'P'):
+                push += 1
+
+    return [over,under,push]
+
+def get_streaks_teams():
+    matches_spread = soup.find_all('td', class_=["viCellBg1 cellBorderL1 cellTextNorm padLeft","viCellBg2 cellBorderL1 cellTextNorm padLeft"])
+
+    for i in matches_spread:
+        i = i.text.strip()
+        if (len(i) > 0):
+            print(i)
+
+def get_last_streaks():
+    matches = soup.find_all('td', class_=["viCellBg1 cellBorderR1 cellTextNorm padLeft","viCellBg2 cellBorderR1        cellTextNorm padLeft"])
+    for i in matches:
+        i = i.text.strip()
+        break
+
+def perc_totals(values):
+    total_games=values[0] + values [1] + values [2]
+    over = round(values[0]/total_games * 100 ,2 )
+    under = round(values[1]/total_games * 100 , 2)
+    print("Over  % " + str(over))
+    print("Under % " + str(under))
+    print()
+
+def main():
+
+    lista_links =  url_loop()
+    for i in lista_links:
+        values =[]
+        soup = format_url(i.strip())
+        values= get_totals_teams(soup)
+        perc_totals(values)
+
+
+
+
+main()
+

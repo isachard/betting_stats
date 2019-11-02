4c4d8331c5793514c830d9c0cfa191be269bbc5a
isachard <isachard.rodriguez@pcc.edu>
2019-11-01 17:24:23 -0700

    fixed issues with percetanges, request to the servers

--- nba/nba_trends.py
+++ nba/nba_trends.py
@@ -3,8 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-teamdb = 55
-
 def url_loop():
     file = open("links.txt","r")
     links = file.readlines()
@@ -14,74 +12,36 @@ def url_loop():
 def format_url(_url):
 
     #put this in a function!
-    team_name = _url[57].upper() + _url[58:]
+
+    team_name = _url[58].upper() + _url[59:]
     print(team_name)
     result = requests.get(_url)
     content = result.content
     soup = BeautifulSoup(content, features="html.parser")
     return soup
 
-def caller():
-    soup = format_url()
-    lista = get_totals_team(soup)
-    print(lista)
-
-def get_totals_teams(soup):
-    over = 0
-    under = 0
-    push = 0
-
+def get_streak(soup):
+    win = lose = push = 0
     matches = soup.find_all('td', class_=["viCellBg1 cellBorderR1 cellTextNorm padLeft","viCellBg2 cellBorderR1 cellTextNorm padLeft"])
     for i in matches:
-
         i = i.text.strip()
         if (len(i) > 0):
             if(i[0] == 'W'):
-                over +=1
+                win +=1
             if(i[0] == 'L'):
-                under +=1
+                lose +=1
             if(i[0] == 'P'):
                 push += 1
 
-    return [over,under,push]
-
-def get_streaks_teams():
-    matches_spread = soup.find_all('td', class_=["viCellBg1 cellBorderL1 cellTextNorm padLeft","viCellBg2 cellBorderL1 cellTextNorm padLeft"])
-
-    for i in matches_spread:
-        i = i.text.strip()
-        if (len(i) > 0):
-            print(i)
-
-def get_last_streaks():
-    matches = soup.find_all('td', class_=["viCellBg1 cellBorderR1 cellTextNorm padLeft","viCellBg2 cellBorderR1        cellTextNorm padLeft"])
-    for i in matches:
-        i = i.text.strip()
-        break
-
-def perc_totals(values):
-
-    total_games=values[0] + values [1] + values [2]
-    
-    over = values[0]/total_games   
-    under =values[1]/total_games  
-    print("Over  % " + str(over))
-    print("Under % " + str(under))
-    print(str(values[0]) + "   -   "  +str(values[1]) ) 
-    print()
+    return [win,lose, push]
 
 def main():
-
-    lista_links =  url_loop()
-    for i in lista_links:
+    links = url_loop()
+    for i in links:
         values =[]
         soup = format_url(i.strip())
+        values = get_streak(soup)
         print(values)
-        values= get_totals_teams(soup)
-        perc_totals(values)
-
-
-
+        print()
 
 main()
-

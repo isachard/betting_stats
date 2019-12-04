6ddb1daf2f50292caa492d9a9743a2801566e5b7
isachard <isachard.rodriguez@pcc.edu>
2019-12-04 14:42:50 -0800

    Added nba lines and fixed consensus lines

--- lines/lines_nba.py
+++ lines/lines_nba.py
@@ -6,7 +6,7 @@ lista = []
 
 def url_format():
 
-    _url = 'https://www.sportsline.com/sportsline-web/service/v1/odds?league=nhl&auth=1'
+    _url = 'https://www.sportsline.com/sportsline-web/service/v1/odds?league=nba&auth=1'
     result = requests.get(_url)
     content = result.content
     content = content.decode('utf8').replace("'",'"')
@@ -21,21 +21,22 @@ def list_of_lines(data):
     current = []
     for i in data['competitions']:
         if 'currentTotal' in i['sportsbookOdds'][0]:
-            values_to_list([i['awayTeamName'], i['homeTeamName'], i['sportsbookOdds'][1]['openingTotal'],i['sportsbookOdds'][1]['currentTotal']])
-
+            values_to_list([i['awayTeamName'],i['homeTeamName'],
+                i['sportsbookOdds'][1]['openingPointSpreadHomeHandicap'],i['sportsbookOdds'][1]['currentPointSpreadHomeHandicap'],
+                i['sportsbookOdds'][1]['openingTotal'], i['sportsbookOdds'][1]['currentTotal']])
 
 def values_to_list(values):
     global lista
-    lista.append([values[0],values[1],values[2],values[3]])
+    lista.append([values[0],values[1],values[2],values[3],values[4],values[5]])
 
 
 def csv_writer(values):
     global lista
     with open('nhl_current.csv', mode='w') as current_trends:
         nhl_writer = csv.writer(current_trends, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        nhl_writer.writerow(["Away", "Vs", "Home", "Opening_Line","Current_Line"])
+        nhl_writer.writerow(["Away", "Vs", "Home", "Opening_Spread_Home", "Current_Spread_Home" ,"Opening_Line","Current_Line"])
         for i in lista :
-            nhl_writer.writerow([i[0],"~",i[1],i[2],i[3] ])
+            nhl_writer.writerow([i[0],"~",i[1],i[2],i[3],i[4],i[5] ])
 
 def main():
 

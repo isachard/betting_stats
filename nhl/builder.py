17a53e6977bcb2fc5c1693ed69063c32bb889765
Isachard Rodriguez <isachard.rodriguez@pcc.edu>
2020-03-03 12:34:05 -0800

    Update builder.py

--- nhl/builder.py
+++ nhl/builder.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 01:09:04 2020
+
+@author: shka
+"""
+
 import requests
 
 from datetime import datetime
 from bs4 import BeautifulSoup
-from tabulate import tabulate
+
 import csv
 
 
@@ -24,18 +31,39 @@ def format_url(_url):
     soup = BeautifulSoup(content, features="html.parser")
     return soup
 
-def caller():
-    soup = format_url()
-    lista = get_totals_team(soup)
-    print(lista)
+def get_totals_streaks(soup):
+    lista=[]
+    over = 0
+    under = 0
 
-def get_totals_teams(soup):
+    matches = soup.find_all('td', class_=["viCellBg1 cellBorderR1 cellTextNorm padLeft","viCellBg2 cellBorderR1 cellTextNorm padLeft"])
+    for i in range(len(matches)):
+        matches[i] = matches[i].text.strip()
+        if (len(matches[i]) > 0): 
+            lista.append(matches[i]) 
+    lista.reverse()
+    for i in range(len(lista)):     
+                  
+        if lista[i][0] == 'U':
+            under = under + 1
+            if lista[i+1][0] != 'U':
+                return "Under: " +str(under)
+            
+        if lista[i][0] == 'O':
+            over = over + 1
+            
+            if lista[i+1][0] != 'O':
+                return "Over: " + str(over)
+        
+
+def get_totals_trends(soup):
     over = 0
     under = 0
     push = 0
 
     matches = soup.find_all('td', class_=["viCellBg1 cellBorderR1 cellTextNorm padLeft","viCellBg2 cellBorderR1 cellTextNorm padLeft"])
     for i in matches:
+        
 
         i = i.text.strip()
         if (len(i) > 0):
@@ -50,22 +78,19 @@ def get_totals_teams(soup):
 
 def csv_writer():
     global lista
-
-
-
     with open('nhl_current.csv', mode='w') as current_trends:
 
         nhl_writer = csv.writer(current_trends, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        nhl_writer.writerow(["Teams", "Over", "Under", "Push","Over %", "Under %"])
+        nhl_writer.writerow(["Teams", "Over", "Under", "Push","Over %", "Under %", "Streaks"])
         for item in lista :
             total = item[1] + item[2]
             over = round((item[1] / total) * 100,1)
             under = round((item[2] / total) * 100,1)
-            nhl_writer.writerow([item[0],item[1],item[2],item[3],over,under])
+            nhl_writer.writerow([item[0],item[1],item[2],item[3],over,under, item[4]])
 
-def team_values_to_list(team,values):
+def team_values_to_list(team,trends,streaks):
     global lista
-    lista.append([team[-1:], values[0],values[1],values[2]])
+    lista.append([team[-1:], trends[0],trends[1],trends[2],streaks])
     print(team[-1:])
 
 def main():
@@ -75,11 +100,11 @@ def main():
     for i in lista_links:
         values =[]
         soup = format_url(i.strip())
-        values= get_totals_teams(soup)
-        team_values_to_list(teams_nhl, values)
+        trends_values= get_totals_trends(soup)
+        streaks_values = get_totals_streaks(soup)
+        team_values_to_list(teams_nhl, trends_values, streaks_values)
     csv_writer()
 
 
 
 main()
-
